f6976856b36cb48294c2e425c8884a7b5a9e1d2c
Md Shohag Mia <shohag.mia@datasoft-bd.com>
2021-06-10 15:29:35 +0600

    DataServices

--- SubReportInRDLC/Controllers/PDFController.cs
+++ SubReportInRDLC/Controllers/PDFController.cs
@@ -9,17 +9,19 @@ namespace SubReportInRDLC.Controllers
 {
     public class PDFController : Controller
     {
-        // GET: PDF/SubReports2008
-        public ActionResult SubReports2008()
+        #region DataServices
+        public readonly List<Department> listOfDepartmentsMainReports;
+        public readonly List<Employee> listOfEmployeeSubReports;
+        #endregion
+        public PDFController()
         {
-            #region DataServices
-            var listOfDepartmentsMainReports = new List<Department>()
+            listOfDepartmentsMainReports = new List<Department>()
             {
                 new Department(){Id=1, Name="CSE"},
                 new Department(){Id=2, Name="EEE"},
                 new Department(){Id=3, Name="ETE"},
             };
-            var listOfEmployeeSubReports = new List<Employee>()
+            listOfEmployeeSubReports = new List<Employee>()
             {
                 new Employee(){Id=1, DepartmentId=1, Name="XX1", Email="x1@gmail.com"},
                 new Employee(){Id=2, DepartmentId=1, Name="XX2", Email="x2@gmail.com"},
@@ -28,9 +30,11 @@ namespace SubReportInRDLC.Controllers
                 new Employee(){Id=5, DepartmentId=2, Name="XX5", Email="x4@gmail.com"},
                 new Employee(){Id=6, DepartmentId=3, Name="XX6", Email="x4@gmail.com"},
             };
+        }
 
-            #endregion
-
+        // GET: PDF/SubReports2008
+        public ActionResult SubReports2008()
+        {
             const string reportName = "Test Sub Reports In RDLC";
             const string reportPath = "~/RDLC/";
             const string rdlcName = "MainReports2008.rdlc";
@@ -48,28 +52,10 @@ namespace SubReportInRDLC.Controllers
             var renderedBytes = ReportUtility.RenderedReportViewer(reportViewer, "PDF", out mimeType, reportName);
             return File(renderedBytes, mimeType);
         }
+
         // GET: PDF/SubReports2016
         public ActionResult SubReports2016()
         {
-            #region DataServices
-            var listOfDepartmentsMainReports = new List<Department>()
-            {
-                new Department(){Id=1, Name="CSE"},
-                new Department(){Id=2, Name="EEE"},
-                new Department(){Id=3, Name="ETE"},
-            };
-            var listOfEmployeeSubReports = new List<Employee>()
-            {
-                new Employee(){Id=1, DepartmentId=1, Name="XX1", Email="x1@gmail.com"},
-                new Employee(){Id=2, DepartmentId=1, Name="XX2", Email="x2@gmail.com"},
-                new Employee(){Id=3, DepartmentId=2, Name="XX3", Email="x3@gmail.com"},
-                new Employee(){Id=4, DepartmentId=2, Name="XX4", Email="x4@gmail.com"},
-                new Employee(){Id=5, DepartmentId=2, Name="XX5", Email="x4@gmail.com"},
-                new Employee(){Id=6, DepartmentId=3, Name="XX6", Email="x4@gmail.com"},
-            };
-
-            #endregion
-
             const string reportName = "Test Sub Reports In RDLC";
             const string reportPath = "~/RDLC/";
             const string rdlcName = "MainReports2016.rdlc";
@@ -87,7 +73,7 @@ namespace SubReportInRDLC.Controllers
             var renderedBytes = ReportUtility.RenderedReportViewer(reportViewer, "PDF", out mimeType, reportName);
             return File(renderedBytes, mimeType);
         }
-      
+
         private void ReportViewer_SubreportProcessing(object sender, SubreportProcessingEventArgs e, List<Employee> listOfEmployeeSubReports)
         {
             var departmentId = Convert.ToInt32(e.Parameters[0].Values[0]);
