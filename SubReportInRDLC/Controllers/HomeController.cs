f57afd802cdd8d1880d0616563875ae8c05fafdf
MD SHOHAG MIA <eshohag@outlook.com>
2021-06-26 17:13:04 +0600

    Updates code

--- SubReportInRDLC/Controllers/HomeController.cs
+++ SubReportInRDLC/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace SubReportInRDLC.Controllers
 {
@@ -25,6 +21,6 @@ namespace SubReportInRDLC.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
-        }   
+        }
     }
 }
